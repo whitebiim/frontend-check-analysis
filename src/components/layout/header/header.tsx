784812efc9a420059a35b28ab5eb46cdd37078df
whitebiim <anyabulbarova@bk.ru>
2025-08-21 13:24:25 +0300

    fix

--- src/components/layout/header/header.tsx
+++ src/components/layout/header/header.tsx
@@ -1,11 +1,17 @@
 import React from 'react';
 import './header.css';
+import Icon from '../../../assets/icons/name/bg.svg';
 
 export const Header = ({ pageTitle }: { pageTitle: string }) => {
   return (
     <header className="header">
-      <h1 className="header-title">{pageTitle}</h1>
-      <div className="company-name">Название название</div>
+      <div className="header-content">
+        <h1 className="header-title">{pageTitle}</h1>
+      </div>
+      <div className="company-container">
+        <img src={Icon} alt="Иконка" className="header-icon" />
+        <div className="company-name">Название название</div>
+      </div>
     </header>
   );
 };
